b426ea431cfc6fe70c7e0758e46fccf34b52d52e
涂奕恒 <412824651@qq.com>
2017-06-07 17:13:50 +0800

    删除冗余信息

--- src/main/java/com/sojson/common/timer/ToTimer.java
+++ src/main/java/com/sojson/common/timer/ToTimer.java
@@ -17,14 +17,15 @@ import com.sojson.permission.service.RoleService;
 @Component
 public class ToTimer{
 	
-	@Resource
-	RoleService roleService;
-	@Scheduled(cron = "0/20 * * * * ? ")
+	/*@Resource
+	RoleService roleService;*/
+	@Scheduled(cron = "0/1 * * * * ? ")
 	public void run() {
 		/**
 		 * 调用存储过程，重新创建表，插入初始化数据。
 		 */
-		roleService.initData();
+		System.out.println("调用存储过程，重新创建表，插入初始化数据。");
+		/*roleService.initData();*/
 		System.out.println(new Date().getTime());
 	}
 
